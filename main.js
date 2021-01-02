a90cb1e75b2166f057f0c281b9479c7ce5f13581
Thaniksha <73345437+Thaniksha@users.noreply.github.com>
2021-01-02 10:57:45 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+function getParagraph1(){
+var inputs=[];
+for(var i = 1;i<=6;i++){
+    inputs.push(document.getElementById("para1_input_box_"+i).value);
+}
+inputs.join("  . ");
+document.getElementById("showParagraph1").innerHTML=inputs.join(" . ")
+
+}
+
+function getParagraph2(){
+    var input1=[];
+    for(var i = 1;i<=6;i++){
+        input1.push(document.getElementById("para2_input_box_"+i).value);
+    }
+    inputs.join("  . ");
+    document.getElementById("showParagraph2").innerHTML=input1.join(" . ")
